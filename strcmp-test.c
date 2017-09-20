14750846a44f12c4a876a01278f2d85644764c47
LuuucyXuuu <xu6@cooper.edu>
2017-09-20 19:02:53 -0400

    empty

--- strcmp-test.c
+++ strcmp-test.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 
-int main() 
+int main(int argc, char *argv[]) 
 {
+
+    /*for (int i = 1; i < argc; ++i)
+    	{
+    	char c = argv [i][0];
+    	switch (c) {
+    		case('1'):
+    			printf("one\n");
+    			break;
+    		case('2'):
+    			printf("two\n");
+    			break;
+    		default:
+    			printf("default\n");
+    	}
+    }*/
+    for (int i = 1; i < argc; ++i){
+    	if(strcmp(argv[i], "1") == 0) {
+			printf("one\n");
+		} else if (strcmp(argv[i], "2") == 0)
+			printf("two\n")
+    }
     return 0;
 }
+
 
